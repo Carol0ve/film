e77d9185c5b4757a2b4f6a21c3a1496761cf40b3
bjWu450881 <1601380665@qq.com>
2024-01-15 21:46:09 +0800

    前端格式优化

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,46 +1,47 @@
 //获取登录对象并绑定点击事件
-const login = document.querySelector('.btn')
-login.addEventListener('click', function () {
-    event.preventDefault();
+const login_btn = document.querySelector('.login_btn')
+login_btn.addEventListener('click', function(){
+  event.preventDefault();
 
-    //获取输入的用户名和密码
-    const account = document.querySelector('.account')
-    const password = document.querySelector('.password')
+  //获取输入的用户名和密码
+  const login_account = document.querySelector('.login_account')
+  const login_password = document.querySelector('.login_password')
 
-    const userInfo = {
-        account: account.value,
-        password: password.value
-    };
+  const login_userInfo = {
+    account: login_account.value,
+    password: login_password.value
+  };
 
-    if (account.value && password.value) {
-        $.ajax({
-            url: '/user/login',
-            type: 'post',
-            contentType: 'application/json;charset=utf-8',
-            data: JSON.stringify(userInfo),
-            success: function (result) {
-                // 处理成功的结果或执行成功时的操作
-                if (result.code === 1) {
+  if(login_account.value && login_password.value){
+    $.ajax({
+      url: '/user/login',
+      type: 'post',
+      contentType: 'application/json;charset=utf-8',
+      data: JSON.stringify(login_userInfo),
+      success: function (login_result) {
+          // 处理成功的结果或执行成功时的操作
+          if (login_result.code === 1) {
+            
+              //账号密码正确，将token和账号保存在本地
+              localStorage.setItem('login_token', login_result.map.token)
+              localStorage.setItem('login_account', login_account.value)
+              localStorage.setItem('is_vip', login_result.isVip)
 
-                    //账号密码正确，将token和账号保存在本地
-                    localStorage.setItem('token', result.map.token)
-                    localStorage.setItem('account', account.value)
-
-                    //登录成功，跳转到主页面
-                    window.location.href = './index.html';
-                } else {
-                    alert("登录失败！");
-                }
-            },
-            error: function (xhr, status, error) {
-                // 处理请求失败的情况
-                console.error("Request failed:", status, error);
-                alert("请求失败，请检查网络或服务器配置。");
-            }
-        });
-    } else {
-        alert('用户名或密码不能为空！')
-    }
+              //登录成功，跳转到主页面
+              window.location.href = './index.html';
+          } else {
+              alert("登录失败！");
+          }
+      },
+      error: function (xhr, status, error) {
+          // 处理请求失败的情况
+          console.error("Request failed:", status, error);
+          alert("请求失败，请检查网络或服务器配置。");
+      }
+  });
+  }else{
+    alert('用户名或密码不能为空！')
+  }
 
 })
 
--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,45 +1,47 @@
 //获取登录对象并绑定点击事件
-const login = document.querySelector('.btn')
-login.addEventListener('click', function () {
-    event.preventDefault();
-    const email = document.querySelector('.email')
-    const username = document.querySelector('.username')
-    const account = document.querySelector('.account')
-    const password = document.querySelector('.password')
-    const passwordagin = document.querySelector('.passwordagin')
+const register_btn = document.querySelector('.register_btn')
+register_btn.addEventListener('click', function(){
+  event.preventDefault();
+  const register_email = document.querySelector('.register_email')
+  const register_username = document.querySelector('.register_username')
+  const register_account = document.querySelector('.register_account')
+  const register_password = document.querySelector('.register_password')
+  const register_passwordagin = document.querySelector('.register_passwordagin')
 
-    const userInfo = {
-        email: email.value, username: username.value, account: account.value, password: password.value,
-    };
-
-    //判断是否存在未输入
-    if (email.value && username.value && account.value && password.value && passwordagin.value) {
-        // 判断密码是否一致
-        if (password.value === passwordagin.value) {
-            $.ajax({
-                url: '/user/register',
-                type: 'post',
-                contentType: 'application/json;charset=utf-8',
-                data: JSON.stringify(userInfo),
-                success: function (result) {
-                    // 处理成功的结果或执行成功时的操作
-                    if (result.code === 1) {
-                        //注册成功，跳转到登录界面
-                        window.location.href = './login.html';
-                    } else {
-                        alert("注册失败！请尝试重新注册");
-                    }
-                },
-                error: function (xhr, status, error) {
-                    // 处理请求失败的情况
-                    console.error("Request failed:", status, error);
-                    alert("请求失败，请检查网络或服务器配置。");
-                }
-            });
-        } else {
-            alert('两次密码不一致')
-        }
-    } else {
-        alert('不能为空')
+  const register_userInfo = {
+    email: register_email.value,
+    username: register_username.value,
+    account: register_account.value,
+    password: register_password.value,
+  };
+  //判断是否存在未输入
+  if(register_email.value && register_username.value && register_account.value && register_password.value && register_passwordagin.value){
+    // 判断密码是否一致
+    if(register_password.value === register_passwordagin.value){
+        $.ajax({
+          url:'/user/register',
+          type: 'post',
+          contentType: 'application/json;charset=utf-8',
+          data: JSON.stringify(register_userInfo),
+          success: function (register_result) {
+              // 处理成功的结果或执行成功时的操作
+              if (register_result.code === 1) {
+                  //注册成功，跳转到登录界面
+                  window.location.href = './login_register.html';
+              } else {
+                  alert("注册失败！请尝试重新注册");
+              }
+          },
+          error: function (xhr, status, error) {
+              // 处理请求失败的情况
+              console.error("Request failed:", status, error);
+              alert("请求失败，请检查网络或服务器配置。");
+          }
+      });
+    }else{
+      alert('两次密码不一致')
     }
+  }else{
+    alert('不能为空')
+  }
 })
